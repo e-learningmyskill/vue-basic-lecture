99c6574c8a9a24a04dbb66b3039f7ddb6efe6a24
abdullah <abdullah.zaky@misteraladin.com>
2023-09-04 20:17:45 +0700

    full lecture vue basic

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Vue-Basic
+This is Code for Myskill Learning Video.
+
+By. Abdullah Zaky
---
+++ lecture/video project finished/script/app.js
@@ -0,0 +1,91 @@
+
+const DEFAULT_STATE =  {
+  state:true,
+  inputName:'',
+  names:[],
+  error:'',
+  showError:false,
+  result:''
+}
+
+
+const app = Vue.createApp({
+  data(){
+      return {
+          // ...DEFAULT_STATE,
+          // or something with an object
+          // game: {
+          //   ...DEFAULT_STATE
+          // } but the problem is we need to add more coding to give this.game.inputname or something
+          state:true,
+          inputName:'',
+          names:['Tono','Budi','Ica'],
+          error:'',
+          showError:false,
+          result:''
+      }
+  },
+  computed:{
+      isReady(){
+          return this.names.length > 1;
+      }
+  },
+  methods:{
+      addNameToList(){
+          const userName = this.inputName;
+          if(this.validate(userName)){
+              this.names.push(userName);
+              this.inputName = '';
+              this.showError = false;
+          } else {
+              this.showError = true;
+          }
+      },
+      validate(value){
+          this.error = '';
+          if(value === '' ){
+              this.error = 'Sorry, no empty name';
+              return false;
+          }
+
+          if(this.names.includes(value)){
+              this.error = 'Sorry, names must be unique';
+              return false;
+          }
+          return true
+      },
+      removeName(index){
+          this.names.splice(index,1)
+      },
+      getRandomName(){
+          return this.names[Math.floor(Math.random() * this.names.length)]
+      },
+      generateResult(){
+          let rand = this.getRandomName();//lisa
+
+          if(this.result !== ''){
+              while (rand === this.result){
+                  rand = this.getRandomName();
+              }
+          }
+          this.result = rand;
+      },
+      showResults(){
+          this.generateResult();
+          this.state = false;
+      },
+      resetApp(){
+          this.state = true;
+          this.inputName ='';
+          this.names = [];
+          this.error = '';
+          this.showError = false;
+          this.result = '';
+      },
+      getNewResult(){
+          this.generateResult();
+      }
+  }
+
+
+}).mount('#app');
